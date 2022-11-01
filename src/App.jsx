50ddf0647c30d42d10566e8a956b99b4ab1d0d08
Sushin Pv <sushinpv@gmail.com>
2022-11-01 16:20:59 +0530

    initial version is completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,50 +1,54 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import { invoke } from "@tauri-apps/api/tauri";
-import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
-  const [greetMsg, setGreetMsg] = useState("");
-  const [name, setName] = useState("");
+  let [time, setTime] = useState({
+    hours: 12,
+    minutes: 34,
+    seconds: '00',
+    ampm: "PM",
+  });
 
-  async function greet() {
-    // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-    setGreetMsg(await invoke("greet", { name }));
-  }
+  let configs;
 
+  const updateTime = () => {
+    let currentTime = new Date();
+    let minutes = currentTime.getMinutes() >= 10 ? currentTime.getMinutes() : "0" + currentTime.getMinutes();
+    let hours = currentTime.getHours();
+    let seconds = currentTime.getSeconds() >= 10 ? currentTime.getSeconds() : "0" + currentTime.getSeconds();
+    let ampm = "AM";
+    if (hours >= 12) {
+      hours -= 12;
+      ampm = "PM";
+    }
+    if (hours === 0) hours = 12;
+    hours = hours >= 10 ? hours : "0" + hours;
+
+    setTime({
+      hours,
+      minutes,
+      seconds,
+      ampm,
+    });
+  };
+
+  useEffect(() => {
+    updateTime();
+    let interval = setInterval(updateTime, 1000);
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
   return (
-    <div className="container">
-      <h1>Welcome to Tauri!</h1>
-
-      <div className="row">
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo vite" alt="Vite logo" />
-        </a>
-        <a href="https://tauri.app" target="_blank">
-          <img src="/tauri.svg" className="logo tauri" alt="Tauri logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-
-      <p>Click on the Tauri, Vite, and React logos to learn more.</p>
-
-      <div className="row">
-        <div>
-          <input
-            id="greet-input"
-            onChange={(e) => setName(e.currentTarget.value)}
-            placeholder="Enter a name..."
-          />
-          <button type="button" onClick={() => greet()}>
-            Greet
-          </button>
+    <>
+      <div className="divider"></div>
+      <div className="clock">
+        <div className="hours">
+          <span>{time.ampm}</span>
+          {time.hours}
         </div>
+        <div className="minutes">{time.minutes}</div>
       </div>
-
-      <p>{greetMsg}</p>
-    </div>
+    </>
   );
 }
 
